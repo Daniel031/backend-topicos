eb5706a9157745011ec3baa2f8bfc9617bd3ead9
Dilker15 <dilker72@gmail.com>
2023-06-14 23:49:03 -0400

    agregando ADMINT-LTE3 FINAL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
    
     <title>Document</title>
 </head>
 <body>
+
+
+    <ul>
+        <li><a href="{{ route('login') }}">Login</a></li>
+        <li><a href="{{ route('register') }}">Register</a></li>
+    </ul>   
+
+
+
     
+    
+    <div class="map">
+
+    </div>
+
+
+
+
+   
+    
+    
+
+
+
     
 
 
